2f2bcaefcf31d66ecf097c7aaa4f30de58d272d9
ujohn33 <evgenii.genov@vub.be>
2022-12-19 17:49:24 +0100

    order

--- social_clusters.py
+++ social_clusters.py
@@ -17,6 +17,8 @@ input_dir = 'data/'
 types = ['Zwembad', 'Administratief centrum', 'Cultureel centrum','Museum', 'RVT/WZC/revalidatiecentrum','Technische middelbare school', 'Bibliotheek', 'Sporthal','Academie', 'Stadhuis/Gemeentehuis', 'Ontmoetingscentrum','Andere gebouwen', 'Sportcomplex', 'Algemene middelbare school','Ziekenhuis', 'Lagere school', 'Brandweerkazerne', 'Stadion','Werkplaats', 'OCMW Woningen','Buitengewoon lager onderwijs (MPI)', 'Politiegebouw', 'Jeugdhuis','Dienstencentrum/CAW/dagverblijf','Buitengewoon middelbaar onderwijs (BUSO)', 'Kleuterschool','OCMW Administratief centrum', 'Kast', 'Kinderdagverblijf/BKO/IBO','Laadeiland', 'Voetbalveld', 'Kerk', 'Pomp', 'Andere terreinen','Parking', 'Fontein', 'Tennisveld', 'Containerpark', 'Andere','School', 'Straatverlichting', 'Looppiste', 'Park']
 remove_types = ['Kast', 'Andere gebouwen', 'Andere',  'Andere terreinen', 'Looppiste', 'Laadeiland']
 types = [x for x in types if x not in remove_types]
+# alphabetical order
+types = types.sort()
 cl_A = ['Sporthal', 'Sportcomplex', 'Stadion', 'Voetbalveld']
 cl_B = ['Administratief centrum', 'Stadhuis/Gemeentehuis', 'OCMW Administratief centrum']
 cl_C = ['Lagere school', 'School', 'Kinderdagverblijf/BKO/IBO', 'Algemene middelbare school', 'Technische middelbare school', 'Buitengewoon lager onderwijs (MPI)', 'Buitengewoon middelbaar onderwijs (BUSO)', 'Kleuterschool']
